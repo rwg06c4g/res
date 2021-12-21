b65a4c57bc27ce7c3a050a4165cd36841d097dd1
rwg06c4g <73642692+rwg06c4g@users.noreply.github.com>
2021-12-21 14:59:29 +0800

    Create bytegoofy.js

---
+++ bytegoofy.js
@@ -0,0 +1,9 @@
+<script>
+(function(){
+var el = document.createElement("script");
+el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?c5cd2548b3cccc11de8b917e8edfd23ca3eac4587250c7d3353668311c66125230632485602430134f60bc55ca391050b680e2741bf7233a8f1da9902314a3fa";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window)
+</script>
